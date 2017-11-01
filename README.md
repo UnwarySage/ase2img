9ea306338a36191a8419527fa8af15b1bdf38b26
Eric Stinger <emstin1@protonmail.com>
2017-10-31 20:47:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ase2img
+two scripts for converting ase files to user specified image files using the Aseprite command line interface
+
+# batch_ase2img.sh
+batch_ase2img converts all .ase files in the current directory to the user specified image type.
+    ./batch_ase2img.sh jpg
+the above converts all ase files jpegs
+
+# ase2img.sh
+converts only one file to specified image type.
+    ./ase2img.sh badguy_spr.ase png
+converts aseprite project badguy_spr into a png file.
---
+++ batch_ase2img.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+#Usage batch export ase files to user provided image type
+#eg: ./batch_ase2img.sh jpg
+
+: '
+Copyright (C) 2017  Eric Stinger
+
+ This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'
+
+shopt -s nullglob
+for f in *.ase
+do
+    aseprite -b $f --save-as ${f/ase/$1}
+done
